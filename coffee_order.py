2c0ddb90d56ec70342817f4d7aef324c192457a5
ssu-on <80696703+ssu-on@users.noreply.github.com>
2022-04-22 11:13:32 +0900

    Create coffee_order.py

---
+++ coffee_order.py
@@ -0,0 +1,8 @@
+print("[Coffee Menu]")
+print("1. Amerciano : 1500")
+print("2. Cafe Mocha : 2500")
+print("3.Green tee latte : 3000", end = '\n');print("")
+americano = int(input("Number of americano : "))
+cafemocha = int(input("Number of cafe mocha : "))
+greenteelatte = int(input("Number of green tee latte : "))
+print(" ", end ='\n');print("Total : ",americano * 1500 + cafemocha * 2500 + greenteelatte * 3000)
